f1e3a179391eda693fa4d59cd9bf038757b9b330
Iacopo Sprenger <iacopo.sprenger@proton.me>
2026-04-19 18:18:16 +0200

    chore: update AppTest.php to PHPUnit 10 signatures

--- tests/Integration/AppTest.php
+++ tests/Integration/AppTest.php
@@ -1,29 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace OCA\ImageConverter\Tests\Integration\Controller;
 
+use OCP\App\IAppManager;
 use OCP\AppFramework\App;
 use Test\TestCase;
 
+final class AppTest extends TestCase {
+	private App $app;
 
-/**
- * This test shows how to make a small Integration Test. Query your class
- * directly from the container, only pass in mocks if needed and run your tests
- * against the database
- */
-class AppTest extends TestCase {
+	protected function setUp(): void {
+		parent::setUp();
+		$this->app = new App('imageconverter');
+	}
 
-    private $container;
-
-    public function setUp() {
-        parent::setUp();
-        $app = new App('imageconverter');
-        $this->container = $app->getContainer();
-    }
-
-    public function testAppInstalled() {
-        $appManager = $this->container->query('OCP\App\IAppManager');
-        $this->assertTrue($appManager->isInstalled('imageconverter'));
-    }
-
+	public function testAppInstalled(): void {
+		$appManager = $this->app->getContainer()->get(IAppManager::class);
+		self::assertTrue($appManager->isInstalled('imageconverter'));
+	}
 }
